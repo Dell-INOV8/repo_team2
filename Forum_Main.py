4927dd121c61309c759c5cb9594ccab2008580b2
James Bell <jb282111@hotmail.com>
2018-10-11 13:51:02 -0500

    Update Forum_Main.py

---
+++ Forum_Main.py
@@ -0,0 +1,15 @@
+
+
+
+# this file needs to contain a method for the following:
+# way to create branches
+# an over arching directory for branches
+# a way to name each branch
+# a way to delete each branch (password protected?)
+# take names in from profile for creating comments in branch or to say who made each branch
+# a way to go from branch to branch
+# a way to save contents of branch
+# a way to report comments
+#
+
+
