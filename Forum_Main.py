fb2af3a5374e350aa0ced34728f52cef2adc6573
James Bell <jb282111@hotmail.com>
2018-10-11 14:45:59 -0500

    Update Forum_Main.py
    Adding notes and simple hello world for PyQt

--- Forum_Main.py
+++ Forum_Main.py
@@ -11,5 +11,41 @@
 # a way to save contents of branch
 # a way to report comments
 #
+# Tutorial for PyQt here:
+# https://pythonspot.com/category/pyqt5/
+#
+#
+# widgets = interation to things
+# layouts control the position of the widgets
+# you add widgets to layout and change the layouts as you go
+# 
+#
+#
 
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize
 
+class HelloWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(640, 480))
+        self.setWindowTitle("Hello world")
+
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        gridLayout = QGridLayout(self)
+        centralWidget.setLayout(gridLayout)
+
+        title = QLabel("Hello World from PyQt", self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        gridLayout.addWidget(title, 0, 0)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
